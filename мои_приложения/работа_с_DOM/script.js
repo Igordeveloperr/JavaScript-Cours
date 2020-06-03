ef1569c5f8c7285bd39727ab0a442e96f852db1a
Igor <g.bikmetow@gmail.com>
2020-06-04 01:13:50 +0400

    начал делать анимацию, использу DOM, хз, когда закончу

---
+++ мои_приложения/работа_с_DOM/script.js
@@ -0,0 +1,18 @@
+class Animation{
+    constructor(){
+        this.count = 0;
+        this.startWindow = document.querySelector(".rain");
+    }
+    startRain()
+    {
+        for(let i = 0; i < 3; i++)
+        {
+            const drop = document.createElement("div");
+            this.startWindow.append(drop);
+            drop.classList.add("drop");
+            setTimeout(() => drop.classList.toggle("active"), 100);
+        }    
+    }
+}
+
+new Animation().startRain();
