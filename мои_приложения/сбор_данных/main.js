2c8e7ea3f49b88ff1d38c95482dad059fbbbd22b
Igor <g.bikmetow@gmail.com>
2020-06-24 22:13:51 +0400

    возникла ошибка: не могу получить данные с формы, завтра разберусь

--- мои_приложения/сбор_данных/main.js
+++ мои_приложения/сбор_данных/main.js
@@ -4,10 +4,18 @@ class Form
 {
     constructor()
     {
-        this.nameVal = document.querySelector("#name");
-        this.dateVal = document.querySelector("#date");
-        this.formBtn = document.querySelector("form button");
-        console.log(this.nameVal, this.dateVal, this.formBtn);
+        this.nameVal = document.querySelector("#name"),
+        this.dateVal = document.querySelector("#date"); 
+        this.formBtn = document.querySelector("form button").addEventListener("click", this.getData);
+        this.userObj = {};
+        this.arr = [];
+    }
+
+    getData(e)
+    {   
+        e.preventDefault();
+        console.log(this.nameVal.value);
     }
 }
 
+const test = new Form();
