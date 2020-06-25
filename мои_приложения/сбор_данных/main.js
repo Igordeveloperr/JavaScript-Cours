c0e5cda6df84b4aac53d611e30b4c1578a5f3673
Igor <g.bikmetow@gmail.com>
2020-06-25 12:26:00 +0400

    реализовал функционал добавления данных из формы в локальное хранилище, но валидация самой формы еще страдает, займусь этим позже

--- мои_приложения/сбор_данных/main.js
+++ мои_приложения/сбор_данных/main.js
@@ -4,18 +4,36 @@ class Form
 {
     constructor()
     {
-        this.nameVal = document.querySelector("#name"),
-        this.dateVal = document.querySelector("#date"); 
-        this.formBtn = document.querySelector("form button").addEventListener("click", this.getData);
-        this.userObj = {};
-        this.arr = [];
+        this.name = document.querySelector("#name");
+        this.date = document.querySelector("#date");
+        this.btn = document.querySelector("button");
+        this.userArr = [];
     }
-
-    getData(e)
-    {   
-        e.preventDefault();
-        console.log(this.nameVal.value);
+    workWithData()
+    {
+        this.btn.addEventListener("click", (e) => 
+        {   
+            e.preventDefault();
+            const nameReg = /\W\D/,
+                  dateReg = /\d\d\-\d\d\-\d\d\d\d/;  
+            let resName = this.name.value.match(nameReg),
+                resDate = this.date.value.match(dateReg);
+                
+            if(resName === null || resDate === null)
+            {
+                alert("Неверный формат или пустое поле");
+            }
+            else
+            {   
+                let user = {name:null, date:null};
+                user.name = resName.input;
+                user.date = resDate.input;
+                this.userArr.push(user);
+                localStorage.setItem("users", JSON.stringify(this.userArr));
+            }
+        });
     }
 }
 
 const test = new Form();
+test.workWithData()
