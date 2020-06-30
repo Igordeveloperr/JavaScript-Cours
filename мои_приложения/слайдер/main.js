98c4322384bff48165dc0c57ba716adeff020f29
Gokhlia <g.bikmetow@gmail.com>
2020-06-30 11:36:47 +0300

    доделать

--- мои_приложения/слайдер/main.js
+++ мои_приложения/слайдер/main.js
@@ -1,36 +1,13 @@
-"use strict" // код работает в современном режиме
+let images = document.querySelectorAll(".slider img");
+let count = 0;
 
-class Slider
-{   
-    #prevBtn = document.querySelector("#prev");
-    #nextBtn = document.querySelector("#next");
-    constructor()
+function slider() 
+{
+    for(let i = 0; i < images.length; i++)
     {
-        this._imgArr = document.querySelectorAll(".slider .slids div");
-        this.moveRight = this.slideMoveRight();
-        this.moveRight();
-    }
-    slideMoveRight()
-    {
-        let index = 0;
-        return function()
-        {
-            this.#nextBtn.addEventListener("click", () => 
-            {   
-                if(index >= this._imgArr.length)
-                {
-                    console.log("full");
-                    return false;
-                }
-                else
-                {
-                    this._imgArr[index].style.border = 2 + "px solid gold";
-                    index++;
-                }
-                
-            });
-        }
+        images[i].classList.add("slider-opacity");
     }
 }
 
-const slider = new Slider();
+document.querySelector(".slider").addEventListener("click", slider);
+
