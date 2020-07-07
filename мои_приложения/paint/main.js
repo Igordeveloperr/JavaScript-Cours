3ac0e9b8ae5f4ffad217af9ae5dfc59b09ab44f4
Igor <g.bikmetow@gmail.com>
2020-07-07 21:34:04 +0400

    доделал paint

--- мои_приложения/paint/main.js
+++ мои_приложения/paint/main.js
@@ -2,35 +2,25 @@
 
 const canvas = document.querySelector("#canvas"),
       resetBtn = document.querySelector("#resetAll"),
-      colors = document.querySelectorAll(".colors div");  
-let ctx = canvas.getContext("2d");
+      color = document.querySelectorAll("#color");  
+let ctx = canvas.getContext("2d"),
+    myColor = "#000";
 
-function draw(e)
+canvas.onmousedown = function()
 {
-    let x = e.offsetX,
-        y = e.offsetY;
-    ctx.beginPath();
-    ctx.rect(x, y, 10, 10);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-}
-
-function chooseColor()
-{
-    for(let i = 0; i < colors.length; i++)
-    {   
-        colors[i].addEventListener("click", () => 
-        { 
-            for(let i = 0; i < colors.length; i++){colors[i].style.border = "none";}
-            colors[i].style.border = 2 + "px solid #000";
-            let color = colors[i].getAttribute("color");
-            ctx.fillStyle = color;
-            ctx.strokeStyle = color;
-        });
+    canvas.onmousemove = function(e)
+    {
+        let x = e.offsetX,
+            y = e.offsetY;
+        ctx.beginPath();
+        ctx.rect(x-15, y-15, 10, 10);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
     }
+    canvas.onmouseup = function(){canvas.onmousemove = null;}
 }
 
-chooseColor();
 resetBtn.addEventListener("click", () => ctx.clearRect(0, 0, 800, 800));
-canvas.addEventListener("mousemove", draw.bind());
+
+
