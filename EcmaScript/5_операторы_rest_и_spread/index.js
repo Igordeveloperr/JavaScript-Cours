8f4a1c90af7b432008da18e43de8c16c7ca47b3a
Igor <g.bikmetow@gmail.com>
2020-07-12 23:15:42 +0400

    изучил операторов и объекты

--- EcmaScript/5_операторы_rest_и_spread/index.js
+++ EcmaScript/5_операторы_rest_и_spread/index.js
@@ -5,3 +5,18 @@ console.log(avarege(10,20,30,50))
 // spread
 const array = [1,2,3,5,8,13];
 console.log(...array); // ... - разварачивает каждое значение массива
+
+// дистругтуринг
+const [a,b, ...other] = array;
+console.log(a,b, other);
+
+// с объектами
+const address = {
+    country: "Russia",
+    city: "Moscow",
+    concat: function(){ return `${this.country}; ${this.city}`;}
+}
+console.log(address.concat());
+
+const {city, country} = address;
+console.log(city, country);
---
+++ EcmaScript/6_объекты/index.js
@@ -0,0 +1,6 @@
+const person = {
+    name: "Jack", 
+    age: 26,
+    country: "Russia",
+    toString: function () {return `${this.name}, ${this.age}, ${this.country}`;}
+}
