b139ab093eb6fda7cf1b7b40ef5acbb725ae2745
Igor <g.bikmetow@gmail.com>
2020-08-05 13:44:26 +0400

    немного добавил данных

--- основы_JS/6_циклы/script.js
+++ основы_JS/6_циклы/script.js
@@ -7,3 +7,29 @@ while(a < 16){
     console.log(a * 2 - 2);
     a += 4;
 }
+
+
+const arr = [1,2,3,4,5];
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+for(let a of arr){
+    console.log(a)
+}
+
+
+const dataBase = {
+    host: "localhost",
+    port: 80,
+    dbname: "webDBr",
+    password: "123",
+    getInfo(){
+        console.log(`host:${dataBase.host}`);
+    }
+}
+
+for(let k in dataBase){
+    console.log(`key: ${k}, value: ${dataBase[k]}`);
+}
